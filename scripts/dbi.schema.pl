e2ef8f598793c9831438da47007b060a44eaa36b
ronsavage <ron@savage.net.au>
2013-03-21 10:52:50 +1100

    Add {DBI_SCHEMA} to dbi.schema.pl

--- scripts/dbi.schema.pl
+++ scripts/dbi.schema.pl
@@ -41,7 +41,8 @@ my($attr)              = {};
 $$attr{sqlite_unicode} = 1 if ($ENV{DBI_DSN} =~ /SQLite/i);
 my($dbh)               = DBI -> connect($ENV{DBI_DSN}, $ENV{DBI_USER}, $ENV{DBI_PASS}, $attr);
 
-$dbh -> do('PRAGMA foreign_keys = ON') if ($ENV{DBI_DSN} =~ /SQLite/i);
+$dbh -> do('PRAGMA foreign_keys = ON')           if ($ENV{DBI_DSN} =~ /SQLite/i);
+$dbh -> do("set search_path = $ENV{DBI_SCHEMA}") if ($ENV{DBI_SCHEMA});
 
 my($g) = GraphViz2::DBI -> new(dbh => $dbh, graph => $graph);
 
