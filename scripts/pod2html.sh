385a5add095fc7d371fd4e23b3fc28432a6bb1a9
ronsavage <ron@savage.net.au>
2013-08-20 11:13:18 +1000

    Remove GraphViz2::Parse::Marpa from scripts/pod2html.sh

--- scripts/pod2html.sh
+++ scripts/pod2html.sh
@@ -7,7 +7,6 @@ pod2html.pl -i lib/GraphViz2/DBI.pm              -o $DEST/GraphViz2/DBI.html
 pod2html.pl -i lib/GraphViz2/Utils.pm            -o $DEST/GraphViz2/Utils.html
 pod2html.pl -i lib/GraphViz2/Data/Grapher.pm     -o $DEST/GraphViz2/Data/Grapher.html
 pod2html.pl -i lib/GraphViz2/Parse/ISA.pm        -o $DEST/GraphViz2/Parse/ISA.html
-pod2html.pl -i lib/GraphViz2/Parse/Marpa.pm      -o $DEST/GraphViz2/Parse/Marpa.html
 pod2html.pl -i lib/GraphViz2/Parse/RecDescent.pm -o $DEST/GraphViz2/Parse/RecDescent.html
 pod2html.pl -i lib/GraphViz2/Parse/Regexp.pm     -o $DEST/GraphViz2/Parse/Regexp.html
 pod2html.pl -i lib/GraphViz2/Parse/STT.pm        -o $DEST/GraphViz2/Parse/STT.html
