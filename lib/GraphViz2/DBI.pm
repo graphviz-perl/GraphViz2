25225914c0995db3ff8452c366f70e05dd8affae
ronsavage <ron@savage.net.au>
2014-07-09 16:51:29 +1000

    Add exclude => [qw/table_1 table_2 .../] and include => [qw/table_3 table_4 .../] option to GraphViz2::DBI.create()

--- lib/GraphViz2/DBI.pm
+++ lib/GraphViz2/DBI.pm
@@ -104,6 +104,8 @@ sub create
 
 	delete $$info{$_} for @$exclude;
 
+	# This 'if' stops us excluding all tables :-).
+
 	if ($#$include >= 0)
 	{
 		delete $$info{$_} for grep{! $include{$_} } keys %$info;
@@ -131,7 +133,7 @@ sub create
 
 	for my $table_name (sort keys %$info)
 	{
-		# Make the table name + 'N columns-in-one' be a horizontal record.
+		# Step 1: Make the table name + 'N columns-in-one' be a horizontal record.
 
 		my($label) =
 		[
@@ -147,7 +149,7 @@ sub create
 			};
 		}
 
-		# Make the N columns be a vertical record.
+		# Step 2: Make the N columns be a vertical record.
 
 		$$label[1]{port}        = "{$$label[1]{port}";
 		$$label[$#$label]{text} .= '}';
@@ -314,19 +316,46 @@ This key is optional.
 
 =head1 Methods
 
-=head2 create(name => $name)
+=head2 create(exclude => [], include => [], name => $name)
 
 Creates the graph, which is accessible via the graph() method, or via the graph object you passed to new().
 
 Returns $self to allow method chaining.
 
-$name is the string which will be placed in the root node of the tree. It may be omitted, in which case the root node is omitted.
+Parameters:
+
+=over 4
+
+=item o exclude
+
+An optional arrayref of table names to exclude.
+
+If none are listed for exclusion, I<all> tables are included.
+
+=item o include
+
+An optional arrayref of table names to include.
+
+If none are listed for inclusion, I<all> tables are included.
+
+=item o name
+
+$name is the string which will be placed in the root node of the tree.
+It may be omitted, in which case the root node is omitted.
+
+=back
 
 =head2 graph()
 
 Returns the graph object, either the one supplied to new() or the one created during the call to new().
 
 =head1 FAQ
+
+=head2 Does GraphViz2::DBI work with MySQL/MariaDB databases?
+
+Yes. But see these L<warnings|https://metacpan.org/pod/DBIx::Admin::TableInfo#Description> when using MySQL/MariaDB.
+
+I'm currently using MariaDB V 5.5.38.
 
 =head2 Does GraphViz2::DBI work with SQLite databases?
 
