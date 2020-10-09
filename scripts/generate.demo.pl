63a37d6ce56a7da566a74ae86ef0a18a88caa0f7
Ed J <mohawk2@users.noreply.github.com>
2020-10-09 17:53:31 +0100

    simplify webgen into one file

--- scripts/generate.demo.pl
+++ scripts/generate.demo.pl
@@ -1,10 +1,19 @@
-#!/usr/bin/env perl
-
 use strict;
 use warnings;
+use warnings  qw(FATAL utf8);    # Fatalize encoding glitches.
 
+use GraphViz2::Filer;
 use GraphViz2::Utils;
 
-# ------------------------------------------------
+my $format = shift || die "Usage: $0 svg";
+my $utils = GraphViz2::Utils->new;
+my $outdir = $utils->config->{output_dir};
+my %script = GraphViz2::Filer->new->get_scripts;
 
-GraphViz2::Utils -> new -> generate_demo_index;
+for my $key (sort keys %script) {
+  my $cmd = "perl -Ilib t/$key.t $format $outdir/$key.$format";
+  print "$cmd\n";
+  system $cmd;
+}
+
+$utils->generate_demo_index;
