6560d2509d33ab6ef21d7ab1cb6eb3e755366d0b
Ed J <mohawk2@users.noreply.github.com>
2020-10-09 19:01:56 +0100

    update scripts/extract.node.shapes.pl

--- scripts/extract.node.shapes.pl
+++ scripts/extract.node.shapes.pl
@@ -1,58 +1,18 @@
-#!/usr/bin/env perl
-
 use File::Spec;
-
 use HTML::TreeBuilder;
-
 use HTTP::Tiny;
 
-# --------------------
-
-my($file_name) = File::Spec -> catfile('data', 'node.shapes.html');
-
-if (! -e $file_name)
-{
-	my($page_name) = 'http://www.graphviz.org/doc/info/shapes.html';
-	my($client)    = HTTP::Tiny -> new -> get($page_name);
-
-	if ($$client{success})
-	{
-		open(OUT, '>', $file_name) || die "Can't open(> $file_name): $!";
-		print OUT $$client{content};
-		close OUT;
-	}
-	else
-	{
-		print "Failed to get $page_name: $$client{reason}. \n";
-	}
-}
+my($page_name) = 'http://www.graphviz.org/doc/info/shapes.html';
+my($client)    = HTTP::Tiny->new->get($page_name);
+die "Failed to get $page_name: $$client{reason}. \n" if !$$client{success};
 
 my($root)      = HTML::TreeBuilder -> new();
-my($result)    = $root -> parse_file($file_name) || die "Can't parse: $file_name";
+my($result)    = $root->parse($$client{content}) || die "Can't parse: $file_name";
 my(@node)      = $root -> look_down(_tag => 'table');
-my(@td)        = $node[2] -> look_down(_tag => 'td');
+my @td         = $node[0]->look_down(_tag => 'td');
 
-my(@a);
-my(@content);
-my(@shape);
-my($td);
+my @shape = map [$_->[0]->content_list]->[0], grep @$_, map [$_->look_down(_tag => 'a')], @td;
 
-for $td (@td)
-{
-	@a = $td -> look_down(_tag => 'a');
-
-	next if (! @a);
-
-	@content = $a[0] -> content_list;
-
-	push @shape, $content[0];
-}
-
-$root -> delete();
-
-$file_name = File::Spec -> catfile('data', 'node.shapes.dat');
-
-open(OUT, '>', $file_name) || die "Can't open(> $file_name): $!";
-print OUT map{"$_\n"} sort @shape;
-close OUT;
-
+my $file_name = File::Spec->catfile('data', 'node.shapes.dat');
+open my $fh, '>', $file_name or die "Can't open(> $file_name): $!";
+print $fh map "$_\n", sort @shape;
