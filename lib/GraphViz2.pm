62a9b3cc0948905239837f98171955d365e072c1
Ed J <mohawk2@users.noreply.github.com>
2020-10-31 19:18:50 +0000

    2.59

--- lib/GraphViz2.pm
+++ lib/GraphViz2.pm
@@ -11,7 +11,7 @@ use Moo;
 use IPC::Run3; # For run3().
 use Types::Standard qw/Any ArrayRef HasMethods HashRef Int Str/;
 
-our $VERSION = '2.58';
+our $VERSION = '2.59';
 
 my $DATA_SECTION = get_data_section; # load once
 my $DEFAULT_COMBINE = 1; # default for combine_node_and_port
