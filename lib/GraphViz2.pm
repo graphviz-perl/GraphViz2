c1bf314b84b15723cb1846aaa35e86a76a1a21f2
Ed J <mohawk2@users.noreply.github.com>
2020-11-16 19:06:57 +0000

    use RE to solve quoting maybe-quoted thing

--- lib/GraphViz2.pm
+++ lib/GraphViz2.pm
@@ -435,24 +435,9 @@ sub escape_port {
 
 sub escape_some_chars {
 	my ($s, $quote_chars) = @_;
-	my @s        = split(//, $s);
-	my $label    = '';
-	for my $i (0 .. $#s) {
-		my $maybe = 0;
-		if ($s[$i] eq '"') {
-			if (substr($s, 0, 1) ne '<') {
-				$maybe = 1; # It's not a HTML label
-			}
-		} elsif ($quote_chars and $s[$i] =~ /[$quote_chars]/) {
-			$maybe = 1;
-		}
-		# Escape if not escaped.
-		if ($maybe && (($i == 0) || (($i > 0) && ($s[$i - 1] ne '\\')))) {
-			$label .= '\\';
-		}
-		$label .= $s[$i];
-	}
-	return $label;
+	return $s if substr($s, 0, 1) eq '<'; # HTML label
+	$s =~ s/(\\.)|([$quote_chars])/ defined($1) ? $1 : '\\' . $2 /ge;
+	return $s;
 }
 
 sub log
